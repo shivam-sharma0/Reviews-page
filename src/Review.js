f5c141039fc80e9b6143a6e3a885c18bc784bd1f
shivam-sharma0 <vashistshivam931@gmail.com>
2023-04-23 16:58:37 +0530

    second

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const[index,setIndex]=useState(0);
+  const{name,image,job,text}=people[index];
+  const min=0;
+  const max=people.length-1;
+
+function random() {
+  let newIndex= Math.floor(Math.random() * (people.length-1));
+  if(newIndex===index){
+    newIndex++;
+  }
+  return newIndex
+}
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button
+          className="prev-btn"
+          onClick={() => (index > 0 ? setIndex(index - 1) : setIndex(people.length-1))}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className="next-btn"
+          onClick={() =>
+            index < people.length - 1 ? setIndex(index + 1) : setIndex(0)
+          }
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      <button
+        className="random-btn"
+        onClick={()=>setIndex(random())}
+      >
+        Surprice me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
